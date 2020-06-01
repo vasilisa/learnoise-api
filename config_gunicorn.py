044e42f7c08afe32c295af64689365305cccbb96
vasilisa <vasilisaskv@gmail.com>
2020-06-01 20:50:47 +0100

    passing to gunicorn SWGI server

---
+++ config_gunicorn.py
@@ -0,0 +1,16 @@
+import multiprocessing
+import os 
+workers = 4 # multiprocessing.cpu_count() * 2 + 1
+
+# Gunicorn doesn't like our import of config, so we wrap it in a funciton.
+def from_config_file():
+    global bind, accesslog, errorlog, loglevel
+    from config import config
+    
+    errorlog  = config.get("Server Parameters", "logfile")
+    loglevels = ["debug", "info", "warning", "error", "critical"]
+    loglevel  = loglevels[config.getint("Server Parameters", "loglevel")]
+    
+from_config_file()
+
+print("Logging to:", errorlog)
