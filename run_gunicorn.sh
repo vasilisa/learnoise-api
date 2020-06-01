4beed3c804670d7bea7052131f4728d5bf146f21
vasilisa <vasilisaskv@gmail.com>
2020-06-01 21:01:15 +0100

    passing to gunicorn SWGI server

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -3,8 +3,7 @@ import os
 from configparser import ConfigParser # this is for python 2.7
 
 # Load up config file
-configfilepath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                              'config.txt')
+configfilepath = os.path.join(os.path.dirname(os.path.abspath(__file__)),'config.txt')
 config = ConfigParser()
 config.read(configfilepath)
 
